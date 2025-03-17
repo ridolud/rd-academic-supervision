363fb3da960a9ced99b2f786b6d307e063747e76
ridolud <faridho.lutfi@gmail.com>
2025-03-17 20:44:23 +0700

    create error page

---
+++ error.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+function onClearError() {
+  clearError({ redirect: "/" });
+}
+</script>
+<template>
+  <div class="h-screen w-full flex justify-center items-center">
+    <div class="w-full max-w-96 text-center space-y-4">
+      <h1 class="text-6xl text-gray-400">{{ error?.statusCode }}</h1>
+      <p class="text-gray-400">{{ error?.statusMessage }}</p>
+
+      <UButton @click="onClearError">Kembali kehalaman utama</UButton>
+    </div>
+  </div>
+</template>
